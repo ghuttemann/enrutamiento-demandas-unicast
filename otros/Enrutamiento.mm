2399c4e123a72492280cc0281941c94ad1f135fd
ghuttemann <ghuttemann@bd0bc703-fb3b-0410-a856-e1b9a076a7ce>
2007-10-17 20:20:29 +0000

    Se agrega el archivo de planificación gráfica.

---
+++ otros/Enrutamiento.mm
@@ -0,0 +1,77 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1190856861343" ID="Freemind_Link_1875736442" MODIFIED="1190939964968" STYLE="bubble" TEXT="Enrutamiento de Demandas Unicast">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1190856922515" ID="_" MODIFIED="1190942910187" POSITION="right" TEXT="Definici&#xf3;n del problema" VSHIFT="-128">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1190856975671" ID="Freemind_Link_1970078827" MODIFIED="1191362909843" TEXT="Restricciones" VSHIFT="-70">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1191362869515" HGAP="57" ID="Freemind_Link_30838201" MODIFIED="1191362886187" TEXT="Capacidad enlace" VSHIFT="-42">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1191362889140" HGAP="54" ID="Freemind_Link_264581998" MODIFIED="1191362903765" TEXT="Necesidad demanda" VSHIFT="32">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1190858179437" HGAP="79" ID="Freemind_Link_683037485" MODIFIED="1190944647671" TEXT="Entradas" VSHIFT="-9">
+<font NAME="SansSerif" SIZE="16"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>o Solo grafo NSF?</text>
+</hook>
+</node>
+<node COLOR="#00b439" CREATED="1190858197484" HGAP="84" ID="Freemind_Link_1605463246" MODIFIED="1190939964890" TEXT="Objetivos" VSHIFT="22">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1190860529781" HGAP="32" ID="Freemind_Link_1433987300" MODIFIED="1191362855781" TEXT="Costo" VSHIFT="35">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1190858795062" HGAP="46" ID="Freemind_Link_774811309" MODIFIED="1191352311484" TEXT="Representaci&#xf3;n = Grafo" VSHIFT="88">
+<font NAME="SansSerif" SIZE="16"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>o Es totalmente conexo? = No&#xa;o Es posible cualquier combinaci&#xf3;n de demandas (origen + destino)? = Si&#xa;o Calculo de tablas de rutas de un nodo a todos los dem&#xe1;s = Si</text>
+</hook>
+<node COLOR="#990000" CREATED="1190858808812" HGAP="38" ID="Freemind_Link_843345639" MODIFIED="1190939964937" TEXT="Nodos" VSHIFT="-4">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1190858824578" HGAP="31" ID="Freemind_Link_1493821371" MODIFIED="1190939964937" TEXT="V&#xe9;rtices" VSHIFT="24">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1190858840406" HGAP="31" ID="Freemind_Link_377859729" MODIFIED="1190939964937" TEXT="Pesos (costo, capacidad, tr&#xe1;fico actual)" VSHIFT="25">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1190856955265" ID="Freemind_Link_729660983" MODIFIED="1191362285640" POSITION="left" TEXT="AG" VSHIFT="-177">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1190857003640" HGAP="88" ID="Freemind_Link_1113014635" MODIFIED="1190939964953" TEXT="Cromosomas" VSHIFT="-75">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1190857035171" HGAP="102" ID="Freemind_Link_1582016148" MODIFIED="1191352602828" TEXT="Operador de Cruce" VSHIFT="-2">
+<font NAME="SansSerif" SIZE="16"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>BLX-alfa&#xa;Elitista o guardar solo el mejor</text>
+</hook>
+</node>
+<node COLOR="#00b439" CREATED="1190857052609" HGAP="121" ID="Freemind_Link_9446470" MODIFIED="1190939964953" TEXT="Operador de Mutaci&#xf3;n" VSHIFT="49">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1190857108109" HGAP="90" ID="Freemind_Link_1932890307" MODIFIED="1190940524156" TEXT="Selecci&#xf3;n" VSHIFT="95">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1190857361250" ID="Freemind_Link_927327926" MODIFIED="1191362306015" POSITION="right" TEXT="PSO" VSHIFT="-27">
+<font NAME="SansSerif" SIZE="18"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>Ver tesis de Lima</text>
+</hook>
+<node COLOR="#00b439" CREATED="1191362625406" HGAP="160" ID="Freemind_Link_325484395" MODIFIED="1191362811406" TEXT="Part&#xed;cula" VSHIFT="-57">
+<font NAME="SansSerif" SIZE="16"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>Igual al cromosoma, pero tiene que tener la soluci&#xf3;n actual y la mejor soluci&#xf3;n local</text>
+</hook>
+</node>
+</node>
+</node>
+</map>

8872ae20d42ba56d6a7be4680648caf6787725e1
ghuttemann <ghuttemann@bd0bc703-fb3b-0410-a856-e1b9a076a7ce>
2007-10-17 20:39:40 +0000

    Se agregó el atributo "origen".

--- fuentes/enrutamiento/src/Enlace.h
+++ fuentes/enrutamiento/src/Enlace.h
@@ -1,21 +1,32 @@
 #ifndef ENLACE_H_
 #define ENLACE_H_
 
-class Enlace
-{
+class Enlace {
 private:
-	int destino;
-	double costo;
-	double capacidad;
+    int origen;
+    int destino;
+    double costo;
+    double capacidad;
 public:
-	int getDestino() { return destino; }
-	double getCosto() { return costo; }
-	double getCapaciadad() { return capacidad; }
-	Enlace(int destino, double costo, double capacidad) {
-		this->destino   = destino;
-		this->costo     = costo;
-		this->capacidad = capacidad;
-	}
+    // Getters
+    int getOrigen() { return origen; }
+    int getDestino() { return destino; }
+    double getCosto() { return costo; }
+    double getCapaciadad() { return capacidad; }
+    
+    // Setters
+    void setOrigen(int origen) { this->origen = origen; }
+    void setDestino(int destino) { this->destino = destino; }
+    void setCosto(double costo) { this->costo = costo; }
+    void setCapacidada(double capacidad) {this->capacidad = capacidad; }
+    
+    // Constructor
+    Enlace(int origen, int destino, double costo, double capacidad) {
+        this->origen    = origen;
+        this->destino   = destino;
+        this->costo     = costo;
+        this->capacidad = capacidad;
+    }
 };
 
 #endif /*ENLACE_H_*/

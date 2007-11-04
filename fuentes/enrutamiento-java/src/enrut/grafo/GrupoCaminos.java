cd6828829baa98ab39633c934a81c8d31b8c469d
ghuttemann <ghuttemann@bd0bc703-fb3b-0410-a856-e1b9a076a7ce>
2007-11-04 23:28:07 +0000

    Se agregaron los métodos: vacio, vaciar, eliminar y conservar.

--- fuentes/enrutamiento-java/src/enrut/grafo/GrupoCaminos.java
+++ fuentes/enrutamiento-java/src/enrut/grafo/GrupoCaminos.java
@@ -3,6 +3,7 @@
  */
 package enrut.grafo;
 
+import java.util.Random;
 import java.util.Vector;
 
 
@@ -23,5 +24,56 @@ public class GrupoCaminos {
 	
 	public int getCantCaminos() {
 		return caminos.size();
+	}
+	
+	public Camino eliminar(int i) {
+		return caminos.remove(i);		
+	}
+	
+	public boolean vacio() {
+		return caminos.isEmpty();
+	}
+	
+	public void vaciar() {
+		caminos.clear();
+	}
+	
+	/**
+	 * Elige al azar max caminos, los cuales son
+	 * conservados. El resto (si hubiere) son
+	 * desechados.
+	 * 
+	 * @param max La máxima cantidad de caminos a conservar.
+	 */
+	public void conservar(int max) {
+		int cantCaminos = this.getCantCaminos();
+		
+		/*
+		 * Si hay "max" o menos caminos,
+		 * o si el grupo está vacío, no 
+		 * hacemos nada.
+		 */
+		if (max >= cantCaminos || cantCaminos == 0)
+			return;
+		
+		/*
+		 * Si "max" es cero o negativo, debemos
+		 * vaciar el grupo.
+		 */
+		if (max <= 0) {
+			this.vaciar();
+			return;
+		}
+		
+		/* 
+		 * En cualquier otro caso, eliminamos al 
+		 * azar (cantCaminos-max) caminos.
+		 */
+		Random rand = new Random(System.currentTimeMillis());
+		for (int i=0; i < (cantCaminos - max); i++) {
+			int cantidad = this.getCantCaminos();
+			int elegido = rand.nextInt(cantidad);
+			this.eliminar(elegido);
+		}
 	}
 }

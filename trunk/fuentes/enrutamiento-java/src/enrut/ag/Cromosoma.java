4ce136342ea8ea605a5610c169df89543ec9edad
ghuttemann <ghuttemann@bd0bc703-fb3b-0410-a856-e1b9a076a7ce>
2007-11-05 00:57:47 +0000

    Se agrega el cuerpo a la clase.

--- trunk/fuentes/enrutamiento-java/src/enrut/ag/Cromosoma.java
+++ trunk/fuentes/enrutamiento-java/src/enrut/ag/Cromosoma.java
@@ -3,6 +3,38 @@
  */
 package enrut.ag;
 
+import java.util.Random;
+
 public class Cromosoma {
+	/*
+	 * Genes del cromosoma
+	 */
+	private int[] genes;
 	
+	
+	/*
+	 * Demandas realizadas
+	 */
+	private Demanda[] demandas;
+	
+	
+	/**
+	 * Construye un nuevo cromosoma con alelos
+	 * randómicos para sus genes.
+	 * @param demandas Las demandas solicitadas
+	 */
+	public Cromosoma(Demanda[] demandas) {
+		this.demandas = demandas;
+		genes = new int[demandas.length];
+		
+		/*
+		 * Seteamos los genes del cromosoma con
+		 * alelos randómicos.
+		 */
+		Random rand = new Random(System.currentTimeMillis());
+		for (int i=0; i < genes.length; i++) {
+			int cantCaminos = demandas[i].getCaminos().getCantCaminos();
+			genes[i] = rand.nextInt(cantCaminos);
+		}
+	}
 }

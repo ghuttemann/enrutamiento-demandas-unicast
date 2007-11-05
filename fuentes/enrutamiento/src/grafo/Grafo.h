046114f0e76d4629c231ced17f1dcae5e3026a27
ghuttemann <ghuttemann@bd0bc703-fb3b-0410-a856-e1b9a076a7ce>
2007-11-05 02:13:38 +0000

    Macanadas...

--- fuentes/enrutamiento/src/grafo/Grafo.h
+++ fuentes/enrutamiento/src/grafo/Grafo.h
@@ -11,7 +11,6 @@ private:
 	int longitud;
 	static const int Rmax = 5; // Nro de caminos maximo por demanda 
 public:
-	
 	Grafo();
 	Grafo(int = 20);
 	virtual ~Grafo();
@@ -19,15 +18,14 @@ public:
 	void agregarArista(Arista *);
 	Vertice *obtenerVertice(int);
 	int cantVertices();
-	void generarRutas(int index,int index2, Demanda d[], Camino route[][Rmax]); 
+	void generarRutas(int index, int index2, Demanda d[], Camino route[]); 
 	void generarRutas(Demanda d[], Camino route[][Rmax]); 
 	void reset(Camino);
 	void imprimir();
 	
 	void findroute(Lista *,int from, int to);
-	bool match(Lista *, int , int , double &);
-	bool find(Lista *, int , Arista *);
-	
+	bool match(Lista *, int, int, double&);
+	bool find(Lista *, int, Arista *);
 private:
 	int random(int);
 };

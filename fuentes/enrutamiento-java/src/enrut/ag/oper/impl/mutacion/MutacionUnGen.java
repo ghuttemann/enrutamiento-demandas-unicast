068e31f6f6d797225315f2abaddde0d718eacfaa
ghuttemann <ghuttemann@bd0bc703-fb3b-0410-a856-e1b9a076a7ce>
2007-11-10 04:11:44 +0000

    Se agregan comentarios.

--- fuentes/enrutamiento-java/src/enrut/ag/oper/impl/mutacion/MutacionUnGen.java
+++ fuentes/enrutamiento-java/src/enrut/ag/oper/impl/mutacion/MutacionUnGen.java
@@ -11,11 +11,32 @@ import enrut.ag.oper.OperadorMutacion;
 public class MutacionUnGen implements OperadorMutacion {
 	@Override
 	public void mutar(Cromosoma a) {
-		Random rand     = new Random(System.currentTimeMillis());
-		int cantGenes   = a.getCantGenes();
-		int elegido     = rand.nextInt(cantGenes);
+		Random rand = new Random(System.currentTimeMillis());
+		
+		// Cantidad de genes del cromosoma
+		int cantGenes = a.getCantGenes();
+		
+		// Gen elegido aleatoriamente para mutar
+		int elegido = rand.nextInt(cantGenes);
+		
+		/*
+		 * Dominio sobre el cual puede cambiarse
+		 * el gen, representado por la cantidad
+		 * de caminos disponibles.
+		 */
 		int cantCaminos = a.getDemandas()[elegido].getCaminos().getCantCaminos();
-		int nuevoValor  = rand.nextInt(cantCaminos);
-		a.setGen(elegido, nuevoValor);
+		
+		/*
+		 * Obtenemos el nuevo valor para el gen.
+		 * Mientras el valor calculado sea el
+		 * mismo al actual, volvemos a generarlo.
+		 */
+		int valorActual = a.getGen(elegido);
+		int valorNuevo  = valorActual;
+		while (valorNuevo == valorActual)
+			valorNuevo = rand.nextInt(cantCaminos);
+			
+		// Cambiamos el valor del gen
+		a.setGen(elegido, valorNuevo);
 	}
 }

035fccbf4aaa1730cbaef57dd76e24c86842e605
ghuttemann <ghuttemann@bd0bc703-fb3b-0410-a856-e1b9a076a7ce>
2007-12-07 00:25:52 +0000

    Se agrega esta clase para implementar el movimiento tradicional de las particulas.

---
+++ fuentes/enrutamiento-java/src/enrut/pso/oper/impl/MovimientoTradicional.java
@@ -0,0 +1,16 @@
+/*
+ * @(#)MovimientoTradicional.java
+ */
+package enrut.pso.oper.impl;
+
+import enrut.pso.Particula;
+import enrut.pso.oper.Movimiento;
+
+public class MovimientoTradicional implements Movimiento {
+
+	@Override
+	public int[] mover(Particula p, Particula mejor, int[] factores) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}

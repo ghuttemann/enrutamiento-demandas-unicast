120f07c7c06c8414d60b20f1ecc4c4d4ed54b02a
ghuttemann <ghuttemann@bd0bc703-fb3b-0410-a856-e1b9a076a7ce>
2007-12-26 23:52:15 +0000

    Se agregó/modificó comentarios.

--- trunk/fuentes/enrutamiento-java/src/enrut/Algoritmo.java
+++ trunk/fuentes/enrutamiento-java/src/enrut/Algoritmo.java
@@ -11,13 +11,57 @@ import enrut.utils.Config;
  * igual manera.
  */
 public interface Algoritmo {
+	/**
+	 * Inicializa la población/enjambre con
+	 * la configuración recibida como argumento.
+	 */
 	public void inicializar(Config conf);
+	
+	/**
+	 * Verifica si es necesario reinicializar
+	 * la población/enjambre.
+	 */
 	public boolean reinicializar();
+	
+	/**
+	 * Realiza el proceso de descartar
+	 * cromosomas/partículas iguales.
+	 */
 	public void descartarIguales();
+	
+	/**
+	 * Realiza la evaluación, asignación de
+	 * costos y fitness de cromosomas/particulas.
+	 */
 	public void evaluar();
+	
+	/**
+	 * Realiza las operaciones principales
+	 * del algoritmo en cuestión.
+	 */
 	public void ejecutar();
+	
+	/**
+	 * Obtiene la mejor solución de la
+	 * historia de la población/enjambre.
+	 */
 	public Solucion getMejorSolucion();
+	
+	/**
+	 * Setea la mejor solución de la
+	 * historia de la población/enjambre.
+	 */
 	public void setMejorSolucion(Solucion s);
+	
+	/**
+	 * Obtiene el costo de la mejor solución
+	 * de la historia de la población/enjambre.
+	 */
 	public double getMejorCosto();
+	
+	/**
+	 * Obtiene el fitness de la mejor solución
+	 * de la historia de la población/enjambre.
+	 */
 	public double getMejorFitness();
 }
